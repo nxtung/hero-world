8e10edcc5534d1a5b0783d70e7a94e4788491484
Tung <tungatinternet@gmail.com>
2023-12-05 13:11:24 +0700

    add morgan log & remove unused code

--- index.js
+++ index.js
@@ -2,23 +2,19 @@ import express from 'express'
 import router from './routes/index.js'
 import swaggerDocs from './swagger.js'
 import cors from 'cors'
-
-const myLogger = function (req, res, next) {
-    console.log('LOGGED')
-    next()
-}
+import morgan from 'morgan'
 
 const requestTime = function (req, res, next) {
     req.requestTime = Date.now()
-    console.log(req.requestTime)
+    console.log(new Date(req.requestTime))
     next()
 }
 
 const app = express()
 const port = 3232
 
+app.use(morgan('dev'))
 app.use(cors())
-
 app.use(express.json())
 app.use(requestTime)
 app.use(router)
